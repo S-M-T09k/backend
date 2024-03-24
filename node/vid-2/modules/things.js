0111db480756462a17e2bc070042f2840d35fbd5
S-M-T09k <S.M.T.09k@gmail.com>
2024-03-24 14:44:06 +0630

    lesson-1 (basics and fs)

---
+++ node/vid-2/modules/things.js
@@ -0,0 +1,7 @@
+const names = ['mark', 'lilly', 'lucy', 'peter', 'sarah'];
+const times = ['10am', '11am', '12pm', '1pm', '2pm'];
+
+module.exports = { names, times };
+
+//!don't do this
+//! export default names;
