2b0a86566a8facb409cc54415c105594a0af155c
S-M-T09k <S.M.T.09k@gmail.com>
2024-03-25 08:26:50 +0630

    lesson-3 (sending html web pages, routing, and redirecting)

---
+++ node/vid-4/pages/index.js
@@ -0,0 +1,17 @@
+console.log('Hello world');
+
+document.querySelector('button').addEventListener('click', () => {
+  console.log('clicked');
+  // fetch('http://localhost:5000/', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.stringify({
+  //     name: 'Mark',
+  //     age: 25,
+  //     work: 'Engineer',
+  //     location: 'London'
+  //   })
+  // })
+});
---
+++ node/vid-4/server.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+
+  console.log('request made');
+  console.log(req.url, req.method);
+
+  let filePath = path.join(__dirname, './pages');
+  res.setHeader('Connect-type', 'text/html');
+
+  switch (req.url) {
+    case '/':
+      filePath += '/index.html';
+      break;
+    case '/about':
+      filePath += '/about.html';
+      break;
+    case '/home':
+      res.setHeader('Location', '/');
+      res.end();
+    case '/style.css':
+      filePath += '/style.css';
+      res.setHeader('Content-type', 'text/css');
+      break;
+    case '/index.js':
+      filePath += '/index.js';
+      res.setHeader('Content-type', 'text/javascript');
+      break;
+    default:
+      filePath += '/404.html';
+      res.statusCode = 404;
+      break;
+  }
+
+  //*respond with a html page
+  fs.readFile(filePath, (err, data) => {
+    if (err) throw err, res.end(err);
+    res.end(data);
+  })
+
+})
+
+server.listen(port, 'localhost', () => {
+
+  console.log(`listening for requests on localhost:${port}`);
+
+})
