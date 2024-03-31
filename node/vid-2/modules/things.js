f805e4610f9e7e848c707a36913c2145bcbc88a7
S-M-T09k <S.M.T.09k@gmail.com>
2024-03-31 10:58:32 +0630

    removed node tutorial from this branch

--- node/vid-2/modules/things.js
+++
@@ -1,7 +0,0 @@
-const names = ['mark', 'lilly', 'lucy', 'peter', 'sarah'];
-const times = ['10am', '11am', '12pm', '1pm', '2pm'];
-
-module.exports = { names, times };
-
-//!don't do this
-//! export default names;
